15f3834f8606cc77054079416ef41c78c6f5aa1a
MuhammadAlfiFauzul <alfikerja12@gmail.com>
2025-09-16 02:17:54 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
-local url = "https://raw.githubusercontent.com/WataXScript/WataXMountYahayuk1/main/Loader/main.lua"
+local url = "https://raw.githubusercontent.com/MuhammadAlfiFauzul/scscsc/refs/heads/main/jembut.lua"
 local s, e = pcall(function()
     return loadstring(game:HttpGet(url))()
 end)
